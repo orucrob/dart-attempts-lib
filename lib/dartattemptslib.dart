4e92c4753a8e6d486c0a23c19431ceeba98871b2
orucrob <orucrob@gmail.com>
2014-05-07 09:44:55 +0200

    problem http://stackoverflow.com/questions/20750107/model-class-imported-from-packaged-library solved

--- lib/dartattemptslib.dart
+++ lib/dartattemptslib.dart
@@ -4,6 +4,7 @@ import 'package:observe/observe.dart';
 class MyModel extends Object with Observable{
   @observable String foo;
   String toString(){
+    //uff
     return "MyModel[foo:$foo]";
   }
 }
